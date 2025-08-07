e693d8fe6a2eb0cc65d156c945b1e0ce636fcb49
RajUjjwal30 <your.email@example.com>
2025-08-07 17:01:33 +0530

    Configured Spring Security

--- src/main/java/org/example/userservice/Configurations/SpringSecurityConfiguration.java
+++ src/main/java/org/example/userservice/Configurations/SpringSecurityConfiguration.java
@@ -7,22 +7,22 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> {
-                            try {
-                                requests
-                                        .anyRequest().permitAll()
-                                        .and().cors().disable()
-                                        .csrf().disable();
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> {
+//                            try {
+//                                requests
+//                                        .anyRequest().permitAll()
+//                                        .and().cors().disable()
+//                                        .csrf().disable();
+//                            } catch (Exception e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//
+//        return http.build();
+ //   }
 
 }
